010ffcf219b20c21af5bdfb6bb76bd1b68fac8c4
Salvador Garcia-Munoz <sgarciam@ic.ac.uk>
2020-05-27 23:05:16 -0400

    Updated May 27 2020
    Added example on using NLP option for PLS model building with missing data

--- examples/Example_Script_testing_MD_by_NLP.py
+++ examples/Example_Script_testing_MD_by_NLP.py
@@ -11,8 +11,9 @@ import pyphi_plots as pp
 
 # Load the data from Excel
 Cars_Features_MD    = pd.read_excel('Automobiles PCA w MD.xls', 'Features', index_col=None, na_values=np.nan)
-Cars_Features    = pd.read_excel('Automobiles PLS.xls', 'Features', index_col=None, na_values=np.nan)
-Cars_CLASSID     = pd.read_excel('Automobiles PCA w MD.xls', 'CLASSID', index_col=None, na_values=np.nan)
+Cars_Performance    = pd.read_excel('Automobiles PLS.xls', 'Performance', index_col=None, na_values=np.nan)
+Cars_Features       = pd.read_excel('Automobiles PLS.xls', 'Features', index_col=None, na_values=np.nan)
+Cars_CLASSID        = pd.read_excel('Automobiles PCA w MD.xls', 'CLASSID', index_col=None, na_values=np.nan)
 
 # Build a PCA model with 3 PC's,
 pcaobj1=phi.pca(Cars_Features_MD,3,md_algorithm='nlp')
@@ -29,3 +30,15 @@ pcaobj3=phi.pca(Cars_Features,3)
 print(pcaobj3['P'])
 pp.score_scatter(pcaobj3,[1,2],CLASSID=Cars_CLASSID,colorby='Cylinders')
 pp.loadings(pcaobj3)
+
+
+plsobj=phi.pls(Cars_Features,Cars_Performance,3)
+pp.r2pv(plsobj)
+pp.score_scatter(plsobj,[1,2],CLASSID=Cars_CLASSID,colorby='Origin',add_ci=True)
+pp.loadings(plsobj)
+
+plsobj_nlp=phi.pls(Cars_Features_MD,Cars_Performance,3,md_algorithm='nlp')
+pp.r2pv(plsobj_nlp)
+pp.score_scatter(plsobj_nlp,[1,2],CLASSID=Cars_CLASSID,colorby='Origin',add_ci=True)
+pp.loadings(plsobj_nlp)
+               
