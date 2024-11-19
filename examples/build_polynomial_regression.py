f5de44c4444fe7a5946f82e3f159cbfabe5f8023
Salvador Garcia-Munoz <salvadorgarciamunoz@gmail.com>
2024-11-18 21:29:52 -0500

    Example on var selection for regression

--- examples/build_polynomial_regression.py
+++ examples/build_polynomial_regression.py
@@ -6,6 +6,7 @@ Created on Sat Nov 16 13:32:27 2024
 """
 import pyphi as phi
 import pandas as pd
+import matplotlib.pyplot as plt
 
 ex_data=pd.read_excel('data.xlsx')
 factors=[
@@ -18,5 +19,24 @@ factors=[
     ]
 
 betas,facts,X,Y=phi.build_polynomial(ex_data,factors,'Response 1')
+plt.figure()
+plt.plot(Y,X@betas,'.')
+plt.xlabel('Observed')
+plt.ylabel('Pred')
+plt.title('Response 1') 
+plt.tight_layout()
 
- 
+newfactors=[ 
+    'Variable A',
+     'VarC',
+     'Variable A*VarC',     
+     'Variable A^2*VarC'
+     ]
+
+betas,facts,X,Y=phi.build_polynomial(ex_data,newfactors,'Response 1')
+plt.figure()
+plt.plot(Y,X@betas,'.')
+plt.xlabel('Observed')
+plt.ylabel('Pred')
+plt.title('Response 1') 
+plt.tight_layout()
