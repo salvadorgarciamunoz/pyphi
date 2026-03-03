34b3d0ca4ff4775a7303f7d19f82b2e46f05c2b2
Salvador Garcia-Munoz <sgarciam@ic.ac.uk>
2026-03-03 16:11:30 -0500

    Update version to 6.0.4

--- pyphi/__init__.py
+++ pyphi/__init__.py
@@ -1,5 +1,5 @@
 """
 pyphi: A Python package for advanced latent variable data analysis: PCA/PLS/JYPLS/JRPLS/TPLS/PLS-CCA (OPLS)
 """
-__version__ = "6.0"
+__version__ = "6.0.4"
 from pyphi import calc, plots, batch
