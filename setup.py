63340b4feb2cf31c489770e952f9251170dfed75
Sal Garcia <salvadorgarciamunoz@gmail.com>
2026-09-21 16:22:47 -0400

    Bump version to 6.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyphi-mvda",
-    version="6.0.5",
+    version="6.0.6",
     license="MIT",
     url="https://salvadorgarciamunoz.github.io/pyphi/",
     author="Sal Garcia",
